ef87219c4f146d40b7ec6ab268ed557a94dc6c32
Devin <devinbud@gmail.com>
2025-03-17 04:22:37 +0000

    completed up to step 2, ready for 3

--- lib/vessel.rb
+++ lib/vessel.rb
@@ -1,9 +1,18 @@
 class Vessel
-  attr_reader :volume
+  attr_reader :name, :volume, :current_volume
 
-  def initialize(name, volume)
+  def initialize(name='FAKE', volume= 100)
     @name = name
     @volume = volume
+    @current_volume = 0 # start with an empty vessel
   end
 
+
+  def empty?
+    @current_volume == 0
+  end
+
+  def fill
+    @current_volume = @volume # assume filling it means it gets filled to full capacity
+  end
 end
